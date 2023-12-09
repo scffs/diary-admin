637c9f9095fc8e82d782403cf72233b751a09940
scffs <khidey@inbox.ru>
2023-12-09 13:14:37 +0700

    feat:add tests

---
+++ apps/api/__tests__/services/user.test.ts
@@ -0,0 +1,48 @@
+import { afterAll, describe, expect, it } from 'bun:test'
+import { PrismaClient } from '@prisma/client'
+import { getAllUsers, getUserById } from '../../src/services/userService'
+import { getRandomID } from '../../src/utils'
+
+const testDb = new PrismaClient()
+
+describe('userService', () => {
+  afterAll(async () => {
+    await testDb.$disconnect()
+  })
+
+  it('getAllUsers should return an array of users', async () => {
+    await testDb.user.createMany({
+      data: [
+        {
+          id: getRandomID(),
+          name: 'User 1',
+          email: 'testuser1@example.com'
+        },
+        {
+          id: getRandomID(),
+          name: 'User 2',
+          email: 'testuser2@example.com'
+        }
+      ],
+      skipDuplicates: true
+    })
+
+    const users = await getAllUsers()
+    expect(users).toHaveLength(2)
+    expect(users[0].name).toBe('User 1')
+    expect(users[1].name).toBe('User 2')
+  })
+
+  it('getUserById should return a user by ID', async () => {
+    const id = getRandomID()
+
+    await testDb.user.create({
+      data: { id: id, name: 'Test User', email: 'testuser3@example.com' }
+    })
+
+    const user = await getUserById(id)
+    expect(user).not.toBeNull()
+    expect(user?.name).toBe('Test User')
+  })
+
+})
