4d79c9f17b6455e2cbbbc49c99456822c193e30e
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-12-09 13:33:43 +0700

    feat: update readme and add tests (#32)
    * feat:add tests
    
    * docs: update readme
    
    * feat: tests
    
    * chore: rm test migration
    
    * Update build-api.yml
    
    * docs: update readme & add path & update gitignore

--- apps/api/src/services/userService.ts
+++ apps/api/src/services/userService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from 'prisma/prisma-client/scripts/default-index'
+import { PrismaClient } from '@prisma/client'
 import { User } from 'shared'
 
 const db = new PrismaClient()
