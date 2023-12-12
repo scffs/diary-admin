b3dc332c9c3ef171ebc2cfb06d1dc95c4841bf23
scffs <khidey@inbox.ru>
2023-12-12 19:57:03 +0700

    feat: update tests

--- apps/api/__tests__/services/auth/auth.test.ts
+++ apps/api/__tests__/services/auth/auth.test.ts
@@ -1,52 +1,51 @@
 import { afterAll, beforeEach, describe, expect, it } from 'bun:test'
 import { PrismaClient } from '@prisma/client'
-import { authenticateUser } from '@services'
+import { authenticateUser, isUserExist } from '@services'
 import { getTestUser } from '@utils'
 
 const testDb = new PrismaClient()
 
 describe('authenticationService', () => {
-  afterAll(async () => {
-    await testDb.$disconnect()
-  })
+	afterAll(async () => {
+		await testDb.$disconnect()
+	})
 
-  beforeEach(async () => {
-    await testDb.user.deleteMany()
-  })
+	beforeEach(async () => {
+		await testDb.user.deleteMany()
+	})
 
-  it('should fail authentication for a non-existent user', async () => {
-    const result = await authenticateUser(
-      1,
-      'nonexistent@example.com',
-      'password'
-    )
-    expect(result.success).toBe(false)
-  })
+	it('should fail authentication for a non-existent user', async () => {
+		const user = await isUserExist(1, 'nonexistent@example.com')
 
-  it('should fail authentication for an invalid password', async () => {
-    const id = 1
-    const testUser = await getTestUser(id)
-    await testDb.user.create({
-      data: testUser
-    })
+		expect(user).toBe(null)
+	})
 
-    const result = await authenticateUser(id, testUser.email, 'wrongpassword')
-    expect(result.success).toBe(false)
-    expect(result.data).toBe('Invalid password')
-  })
+	it('should fail authentication for an invalid password', async () => {
+		const id = 1
+		const testUser = await getTestUser(id)
+		await testDb.user.create({
+			data: testUser
+		})
 
-  it('should authenticate a user with valid credentials', async () => {
-    const id = 1
-    const testUser = await getTestUser(id)
-    await testDb.user.create({
-      data: testUser
-    })
+		const result = await authenticateUser(
+			testUser,
+			testUser.email,
+			'wrongpassword'
+		)
+		expect(result).toBe(false)
+	})
 
-    /**
-     * В getTestUser пароль хешируется, но по задумке authenticateUser мы передаем туда пароль в чистом виде.
-     */
-    const result = await authenticateUser(id, testUser.email, 'password')
-    expect(result.success).toBe(true)
-    expect(result.data).toBe('Password')
-  })
+	it('should authenticate a user with valid credentials', async () => {
+		const id = 1
+		const testUser = await getTestUser(id)
+		await testDb.user.create({
+			data: testUser
+		})
+
+		/**
+		 * В getTestUser пароль хешируется, но по задумке authenticateUser мы передаем туда пароль в чистом виде.
+		 */
+		const result = await authenticateUser(testUser, testUser.email, 'password')
+		expect(result).toBe(true)
+	})
 })
--- apps/api/__tests__/utils/handleErrors/i.test.ts
+++ apps/api/__tests__/utils/handleErrors/i.test.ts
@@ -3,43 +3,43 @@ import { describe, expect, it } from 'bun:test'
 import { handleErrors } from '@utils'
 
 describe('handleErrors', () => {
-  it('should handle VALIDATION error', () => {
-    const result = handleErrors({
-      code: 'VALIDATION',
-      error: new Error('Validation failed')
-    })
+	it('should handle VALIDATION error', () => {
+		const result = handleErrors({
+			code: 'VALIDATION',
+			error: new Error('Validation failed')
+		})
 
-    expect(result.message).toBe('Validation error')
-    expect(result.error).toBeInstanceOf(Error)
-  })
+		expect(result.message).toBe('Validation error')
+		expect(result.error).toBeInstanceOf(Error)
+	})
 
-  it('should handle INTERNAL_SERVER_ERROR', () => {
-    const result = handleErrors({
-      code: 'INTERNAL_SERVER_ERROR',
-      error: new Error('Server error')
-    })
+	it('should handle INTERNAL_SERVER_ERROR', () => {
+		const result = handleErrors({
+			code: 'INTERNAL_SERVER_ERROR',
+			error: new Error('Server error')
+		})
 
-    expect(result.message).toBe('Internal Server Error')
-    expect(result.error).toBeInstanceOf(Error)
-  })
+		expect(result.message).toBe('Internal Server Error')
+		expect(result.error).toBeInstanceOf(Error)
+	})
 
-  it('should handle NOT_FOUND error', () => {
-    const result = handleErrors({
-      code: 'NOT_FOUND',
-      error: new Error('Resource not found')
-    })
+	it('should handle NOT_FOUND error', () => {
+		const result = handleErrors({
+			code: 'NOT_FOUND',
+			error: new Error('Resource not found')
+		})
 
-    expect(result.message).toBe('Not found')
-    expect(result.error).toBeInstanceOf(Error)
-  })
+		expect(result.message).toBe('Not found')
+		expect(result.error).toBeInstanceOf(Error)
+	})
 
-  it('should handle unknown error', () => {
-    const result = handleErrors({
-      code: 'UNKNOWN_ERROR',
-      error: new Error('Unknown error occurred')
-    })
+	it('should handle unknown error', () => {
+		const result = handleErrors({
+			code: 'UNKNOWN_ERROR',
+			error: new Error('Unknown error occurred')
+		})
 
-    expect(result.message).toBe('Unknown error')
-    expect(result.error).toBeInstanceOf(Error)
-  })
+		expect(result.message).toBe('Unknown error')
+		expect(result.error).toBeInstanceOf(Error)
+	})
 })
